7b8e843154a3252f38a34018be9ad26d752ec8b4
Jack Hope <jfhope3000@gmail.com>
2020-03-16 20:43:31 -0500

    part 2, Models complete

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank;
+import javax.persistence.*;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(min = 2, max = 360)
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String name) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
 
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import javax.persistence.*;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message = "Skill is required")
+    @Size(min = 2, max = 360)
+    private String skill;
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setLocation(String name) {
+        this.skill = skill;
+    }
+
+    public Skill() {
+    }
 
 }
